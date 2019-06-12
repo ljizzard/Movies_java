e99283ffb5c12eab933d5a7ef1a0ca248bf240a5
lindsey <lj0978@hotmail.co.uk>
2019-06-12 08:32:28 +0100

    minor tweeks, seems to work

--- src/main/java/com/codeclan/examples/Movies/Components/DataLoader.java
+++ src/main/java/com/codeclan/examples/Movies/Components/DataLoader.java
@@ -1,17 +1,21 @@
 package com.codeclan.examples.Movies.Components;
 
+import com.codeclan.examples.Movies.Repositories.ActorRepository;
 import com.codeclan.examples.Movies.Repositories.DirectorRepository;
 import com.codeclan.examples.Movies.Repositories.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 
-public class DataLoader implements ApplicationRunner {
+public abstract class DataLoader implements ApplicationRunner {
 
     @Autowired
     DirectorRepository directorRepository;
 
     @Autowired
     FilmRepository filmRepository;
+
+    @Autowired
+    ActorRepository actorRepository;
 
 
 
